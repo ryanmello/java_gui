e6ef634dd66874e97482bcc0e16ce695a7c2cb67
Ryan Mello <74434148+ryanmello@users.noreply.github.com>
2023-01-08 19:33:08 +0000

    labels

--- Main.java
+++ Main.java
@@ -1,24 +1,37 @@
-import java.awt.Color;
-import javax.swing.ImageIcon;
-import javax.swing.JFrame;
+import javax.swing.*;
+import java.awt.*;
+import javax.swing.border.Border;
 
 public class Main {  
   public static void main(String[] args) {
+    // MyFrame myFrame = new MyFrame();
 
-    /*
-    JFrame frame = new JFrame(); // creates frame
-    frame.setTitle("JFrame Title"); // sets title of frame
-    frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // exit out of application
-    frame.setResizable(false); // prevent resizing
-    frame.setSize(420, 420); // sets the size of the frame
-    frame.setVisible(true); // makes the frame visible
+    ImageIcon image = new ImageIcon("./img/sbf.jpg");
+    Border border = BorderFactory.createLineBorder(Color.green, 3);
 
-    ImageIcon image = new ImageIcon("./img/sbf.jpg"); // create image icon
-    frame.setIconImage(image.getImage()); // change icon of frame
-    frame.getContentPane().setBackground(new Color(125, 50, 250)); // change color of background
-    */
+    JLabel label = new JLabel();
+    label.setText("Bro, do you even code?"); // set text of label
+    label.setIcon(image);
+    label.setHorizontalTextPosition(JLabel.CENTER); 
+    label.setVerticalTextPosition(JLabel.TOP);
+    label.setForeground(Color.red);
+    label.setFont(new Font("MV Boli", Font.PLAIN, 20));
+    label.setIconTextGap(10); // set gap of text to image
+    label.setBackground(Color.black); // set background color
+    label.setOpaque(true); // display background color
+    label.setBorder(border);
+    label.setVerticalAlignment(JLabel.CENTER); // set vertical position of icon and text
+    label.setHorizontalAlignment(JLabel.CENTER); // set horizontal position of icon and text
+    //label.setBounds(100, 100, 250, 250); // set x and y position within frame
     
-    MyFrame myFrame = new MyFrame();
     
+
+    JFrame frame = new JFrame();
+    frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    //frame.setSize(500, 500);
+    //frame.setLayout(null);
+    frame.setVisible(true);
+    frame.add(label);
+    frame.pack();
   }
 }
