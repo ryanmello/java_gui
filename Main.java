adcac50c9fe516974524deca3f8ddf24a8c8838b
Ryan Mello <74434148+ryanmello@users.noreply.github.com>
2023-01-11 19:48:31 +0000

    flow layout

--- Main.java
+++ Main.java
@@ -8,61 +8,80 @@ public class Main {
     JFrame frame = new JFrame();
     frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     frame.setSize(500, 500);
-    frame.setLayout(new BorderLayout(10, 10));
+    frame.setLayout(new FlowLayout(FlowLayout.CENTER, 10, 10)); 
+
+    JPanel panel = new JPanel();
+    panel.setPreferredSize(new Dimension(250, 250));
+    panel.setBackground(Color.red);
+    panel.setLayout(new FlowLayout());
+
+    panel.add(new JButton("1"));
+    panel.add(new JButton("2"));
+    panel.add(new JButton("3"));
+    panel.add(new JButton("4"));
+    panel.add(new JButton("5"));
+    panel.add(new JButton("6"));
+    panel.add(new JButton("7"));
+    panel.add(new JButton("8"));
+    panel.add(new JButton("9"));
+
+    frame.add(panel);
     frame.setVisible(true);
     
-    JPanel panel1 = new JPanel();
-    JPanel panel2 = new JPanel();
-    JPanel panel3 = new JPanel();
-    JPanel panel4 = new JPanel();
-    JPanel panel5 = new JPanel();
+    
+    
+    // JPanel panel1 = new JPanel();
+    // JPanel panel2 = new JPanel();
+    // JPanel panel3 = new JPanel();
+    // JPanel panel4 = new JPanel();
+    // JPanel panel5 = new JPanel();
 
-    panel1.setBackground(Color.red);
-    panel2.setBackground(Color.green);
-    panel3.setBackground(Color.yellow);
-    panel4.setBackground(Color.magenta);
-    panel5.setBackground(Color.blue);
+    // panel1.setBackground(Color.red);
+    // panel2.setBackground(Color.green);
+    // panel3.setBackground(Color.yellow);
+    // panel4.setBackground(Color.magenta);
+    // panel5.setBackground(Color.blue);
 
-    panel1.setPreferredSize(new Dimension(100, 100));
-    panel2.setPreferredSize(new Dimension(100, 100));
-    panel3.setPreferredSize(new Dimension(100, 100));
-    panel4.setPreferredSize(new Dimension(100, 100));
-    panel5.setPreferredSize(new Dimension(100, 100));
+    // panel1.setPreferredSize(new Dimension(100, 100));
+    // panel2.setPreferredSize(new Dimension(100, 100));
+    // panel3.setPreferredSize(new Dimension(100, 100));
+    // panel4.setPreferredSize(new Dimension(100, 100));
+    // panel5.setPreferredSize(new Dimension(100, 100));
 
-    // ----------------- sub panels -------------- // 
+    // // ----------------- sub panels -------------- // 
 
-    JPanel panel6 = new JPanel();
-    JPanel panel7 = new JPanel();
-    JPanel panel8 = new JPanel();
-    JPanel panel9 = new JPanel();
-    JPanel panel10 = new JPanel();
+    // JPanel panel6 = new JPanel();
+    // JPanel panel7 = new JPanel();
+    // JPanel panel8 = new JPanel();
+    // JPanel panel9 = new JPanel();
+    // JPanel panel10 = new JPanel();
 
-    panel6.setBackground(Color.black);
-    panel7.setBackground(Color.darkGray);
-    panel8.setBackground(Color.gray);
-    panel9.setBackground(Color.lightGray);
-    panel10.setBackground(Color.white);
+    // panel6.setBackground(Color.black);
+    // panel7.setBackground(Color.darkGray);
+    // panel8.setBackground(Color.gray);
+    // panel9.setBackground(Color.lightGray);
+    // panel10.setBackground(Color.white);
 
-    panel5.setLayout(new BorderLayout());
+    // panel5.setLayout(new BorderLayout());
 
-    panel6.setPreferredSize(new Dimension(100, 100));
-    panel7.setPreferredSize(new Dimension(100, 100));
-    panel8.setPreferredSize(new Dimension(100, 100));
-    panel9.setPreferredSize(new Dimension(100, 100));
-    panel10.setPreferredSize(new Dimension(100, 100));
+    // panel6.setPreferredSize(new Dimension(100, 100));
+    // panel7.setPreferredSize(new Dimension(100, 100));
+    // panel8.setPreferredSize(new Dimension(100, 100));
+    // panel9.setPreferredSize(new Dimension(100, 100));
+    // panel10.setPreferredSize(new Dimension(100, 100));
 
-    panel5.add(panel6, BorderLayout.NORTH);
-    panel5.add(panel7, BorderLayout.WEST);
-    panel5.add(panel8, BorderLayout.EAST);
-    panel5.add(panel9, BorderLayout.SOUTH);
-    panel5.add(panel10, BorderLayout.CENTER);
+    // panel5.add(panel6, BorderLayout.NORTH);
+    // panel5.add(panel7, BorderLayout.WEST);
+    // panel5.add(panel8, BorderLayout.EAST);
+    // panel5.add(panel9, BorderLayout.SOUTH);
+    // panel5.add(panel10, BorderLayout.CENTER);
 
-    // ----------------- sub panels -------------- // 
+    // // ----------------- sub panels -------------- // 
 
-    frame.add(panel1, BorderLayout.NORTH);
-    frame.add(panel2, BorderLayout.WEST);
-    frame.add(panel3, BorderLayout.EAST);
-    frame.add(panel4, BorderLayout.SOUTH);
-    frame.add(panel5, BorderLayout.CENTER);
+    // frame.add(panel1, BorderLayout.NORTH);
+    // frame.add(panel2, BorderLayout.WEST);
+    // frame.add(panel3, BorderLayout.EAST);
+    // frame.add(panel4, BorderLayout.SOUTH);
+    // frame.add(panel5, BorderLayout.CENTER);
   }
 }
