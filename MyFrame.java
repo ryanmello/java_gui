55b54e1d67ec05d75e2df499b5610c9ff0d25051
Ryan Mello <74434148+ryanmello@users.noreply.github.com>
2023-01-15 00:40:43 +0000

    combo box

--- MyFrame.java
+++ MyFrame.java
@@ -4,47 +4,37 @@ import java.awt.event.*;
 
 
 public class MyFrame extends JFrame implements ActionListener{
-  
-  JButton button;
-  JCheckBox box;
-
-  JRadioButton pizza;
-  JRadioButton burger;
-  JRadioButton hotdog;
-  
+  JComboBox box;
   MyFrame() {
     this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     this.setLayout(new FlowLayout());
 
-    pizza = new JRadioButton("Pizza");
-    burger = new JRadioButton("Burger");
-    hotdog = new JRadioButton("Hotdog");
+    String[] animals = {"dog", "cat", "bird"};
+    box = new JComboBox(animals);
+    box.addActionListener(this);
 
-    ButtonGroup group = new ButtonGroup();
-    group.add(pizza);
-    group.add(burger);
-    group.add(hotdog);
-
-    pizza.addActionListener(this);
-    burger.addActionListener(this);
-    hotdog.addActionListener(this);
-
-    this.add(pizza);
-    this.add(burger);
-    this.add(hotdog);    
+    //box.getItemCount();
+    //System.out.println(box.getItemCount());
+    box.addItem("horse");
+    box.insertItemAt("pig", 0);
+    box.setSelectedIndex(0);
+    //System.out.println(box.getItemCount());
+    box.removeItem("cat");
+    //System.out.println(box.getItemCount());
+    box.removeItemAt(0);
+    box.removeAll();
+    
+    
+    this.add(box);
     this.pack();
     this.setVisible(true);
   }
   
-  
   @Override
   public void actionPerformed(ActionEvent e){
-    if(e.getSource() == pizza){
-      System.out.println("You ordered pizza");
-    } else if(e.getSource() == burger){
-      System.out.println("You ordered burger");
-    } else if(e.getSource() == hotdog){
-      System.out.println("You ordered hotdog");
+    if(e.getSource() == box){
+      //System.out.println(box.getSelectedItem());
+      System.out.println(box.getSelectedIndex());
     }
   }
 }
