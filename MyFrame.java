1164fc24048c53db2682f02f9ff1848d3d76b280
Ryan Mello <74434148+ryanmello@users.noreply.github.com>
2023-01-16 04:16:24 +0000

    file chooser

--- MyFrame.java
+++ MyFrame.java
@@ -2,54 +2,35 @@ import javax.swing.*;
 import javax.swing.event.*;
 import java.awt.*;
 import java.awt.event.*;
+import java.io.*;
 
 public class MyFrame extends JFrame implements ActionListener{
-  JMenuItem loadItem;
-  JMenuItem saveItem;
-  JMenuItem exitItem;
-  
+  JButton button;
   MyFrame() {
     this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-    this.setSize(500, 500);
     this.setLayout(new FlowLayout());
-    
-    JMenuBar menu = new JMenuBar();
-    JMenu fileMenu = new JMenu("File");
-    JMenu editMenu = new JMenu("Edit");
-    JMenu helpMenu = new JMenu("Help");
 
-    loadItem = new JMenuItem("Load");
-    saveItem = new JMenuItem("Save");
-    exitItem = new JMenuItem("Exit");
+    button = new JButton("Select File");
+    button.addActionListener(this);
 
-    loadItem.addActionListener(this);
-    saveItem.addActionListener(this);
-    exitItem.addActionListener(this);
-
-    loadItem.setMnemonic(KeyEvent.VK_L);
-    saveItem.setMnemonic(KeyEvent.VK_S);
-    exitItem.setMnemonic(KeyEvent.VK_E);
-
-    fileMenu.add(loadItem);
-    fileMenu.add(saveItem);
-    fileMenu.add(exitItem);
-    
-    menu.add(fileMenu);
-    menu.add(editMenu);
-    menu.add(helpMenu);
-    
-    this.setJMenuBar(menu);
+    this.add(button);
+    this.pack();
     this.setVisible(true);
   }
 
   @Override
   public void actionPerformed(ActionEvent e){
-    if(e.getSource() == loadItem){
-      System.out.println("Loading...");
-    } else if(e.getSource() == saveItem){
-      System.out.println("Saving...");
-    } else if(e.getSource() == exitItem){
-      System.exit(0);
+    if(e.getSource() == button){
+      JFileChooser fileChooser = new JFileChooser();
+
+      fileChooser.setCurrentDirectory(new File("."));
+
+      int response = fileChooser.showSaveDialog(null);
+      //System.out.println(file.showOpenDialog(null));
+      if(response == JFileChooser.APPROVE_OPTION){
+        File file = new File(fileChooser.getSelectedFile().getAbsolutePath());
+        System.out.println(file);
+      }
     }
   }
 }
