1d7d52a55b239273b58fc61dde2dd7c78d835490
Ryan Mello <74434148+ryanmello@users.noreply.github.com>
2023-01-17 21:07:32 +0000

    2d animations

--- MyFrame.java
+++ MyFrame.java
@@ -5,9 +5,13 @@ import java.awt.event.*;
 import java.io.*;
 
 public class MyFrame extends JFrame{
+  
   MyPanel panel;
+  
   MyFrame() {
+    
     panel = new MyPanel();
+    
     this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     this.add(panel);
     this.pack();
--- MyPanel.java
+++ MyPanel.java
@@ -4,34 +4,45 @@ import java.awt.*;
 import java.awt.event.*;
 import java.io.*;
 
-public class MyPanel extends JPanel {
+public class MyPanel extends JPanel implements ActionListener{
+
+  final int PANEL_WIDTH = 500;
+  final int PANEL_HEIGHT = 500;
+  Image enemy;
+  Timer timer;
+  int xVelocity = 2;
+  int yVelocity = 1;
+  int x = 0;
+  int y = 0;
+  
   MyPanel() {
-    this.setPreferredSize(new Dimension(500, 500));
+    this.setPreferredSize(new Dimension(PANEL_WIDTH, PANEL_HEIGHT));
+    this.setBackground(Color.white);
+    enemy = new ImageIcon("img/sbf.jpg").getImage();
+    timer = new Timer(10, this);
+    timer.start();
   }
 
   public void paint(Graphics g){
+    super.paint(g); // paint background
     Graphics2D g2D = (Graphics2D) g;
+    g2D.drawImage(enemy, x, y, null);
+    
+  }
 
-    g2D.setPaint(Color.blue);
-    //g2D.setStroke(new BasicStroke(5));
-    //g2D.drawLine(0, 0, 500, 500);
+  public void actionPerformed(ActionEvent e){
 
-    //g2D.setPaint(Color.pink);
-    //g2D.fillRect(0, 0, 100, 200);
-    //g2D.drawRect(0, 0, 100, 200);
+    if(x >= PANEL_WIDTH - enemy.getWidth(null) || x < 0){
+      xVelocity = xVelocity * -1;
+    }
 
-    //g2D.fillOval(0, 0, 100, 100);
-
-    // g2D.setPaint(Color.red);
-    // g2D.fillArc(0, 0, 100, 100, 0, 180);
-    // g2D.setPaint(Color.white);
-    // g2D.fillArc(0, 0, 100, 100, 180, 180);
-
-    // int[] xPoints = {150, 250, 350};
-    // int[] yPoints = {300, 150, 300};
-    // g2D.drawPolygon(xPoints, yPoints, 3);
-
-    g2D.setFont(new Font(null, Font.BOLD, 50));
-    g2D.drawString("WOOOOO", 50, 50);
+    if(y >= PANEL_WIDTH - enemy.getWidth(null) || y < 0){
+      yVelocity = yVelocity * -1;
+    }
+    
+    x = x + xVelocity;
+    y = y + yVelocity;
+    
+    repaint();
   }
 }
