a0a5ede9dfc4ec4b239df8638da7f5f5f2f5924b
Ryan Mello <74434148+ryanmello@users.noreply.github.com>
2023-01-17 05:05:01 +0000

    key binding

--- MyFrame.java
+++ MyFrame.java
@@ -4,55 +4,68 @@ import java.awt.*;
 import java.awt.event.*;
 import java.io.*;
 
-public class MyFrame extends JFrame implements KeyListener{
+public class MyFrame {
 
+  JFrame frame;
   JLabel label;
+  Action upAction;
+  Action downAction;
+  Action leftAction;
+  Action rightAction;
   
   MyFrame() {
-    this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-    this.setSize(500, 500);
-    this.setLayout(null);
-    this.addKeyListener(this);
+    frame = new JFrame("Keybinding Demo");
+    frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    frame.setSize(500, 500);
+    frame.setLayout(null);
 
     label = new JLabel();
-    label.setBounds(0, 0, 100, 100);
     label.setBackground(Color.red);
+    label.setBounds(100, 100, 100, 100);
     label.setOpaque(true);
 
-    this.getContentPane().setBackground(Color.black);
-    this.add(label);
-    this.setVisible(true);
+    upAction = new UpAction();
+    downAction = new DownAction();
+    leftAction = new LeftAction();
+    rightAction = new RightAction();
+
+    label.getInputMap().put(KeyStroke.getKeyStroke('w'), "upAction");
+    label.getActionMap().put("upAction", upAction);
+
+    label.getInputMap().put(KeyStroke.getKeyStroke('s'), "downAction");
+    label.getActionMap().put("downAction", downAction);
+
+    label.getInputMap().put(KeyStroke.getKeyStroke('a'), "leftAction");
+    label.getActionMap().put("leftAction", leftAction);
+
+    label.getInputMap().put(KeyStroke.getKeyStroke('d'), "rightAction");
+    label.getActionMap().put("rightAction", rightAction);
+
+    frame.add(label);
+    frame.setVisible(true);
   }
 
-  @Override
-  public void keyTyped(KeyEvent e){
-    switch(e.getKeyChar()){
-      case 'w': label.setLocation(label.getX(), label.getY() - 10);
-        break;
-      case 'a': label.setLocation(label.getX() - 10, label.getY());
-        break;
-      case 's': label.setLocation(label.getX(), label.getY() + 10);
-        break;
-      case 'd': label.setLocation(label.getX() + 10, label.getY());
-        break;
+  public class UpAction extends AbstractAction {
+    public void actionPerformed(ActionEvent e){
+      label.setLocation(label.getX(), label.getY() - 10);
     }
   }
 
-  public void keyPressed(KeyEvent e){
-    switch(e.getKeyCode()){
-      case 38: label.setLocation(label.getX(), label.getY() - 10); // 87
-        break;
-      case 37: label.setLocation(label.getX() - 10, label.getY()); // 65
-        break;
-      case 40: label.setLocation(label.getX(), label.getY() + 10); // 83
-        break;
-      case 39: label.setLocation(label.getX() + 10, label.getY()); // 68
-        break;
+  public class DownAction extends AbstractAction {
+    public void actionPerformed(ActionEvent e){
+      label.setLocation(label.getX(), label.getY() + 10);
+    }
+  }
+  
+  public class LeftAction extends AbstractAction {
+    public void actionPerformed(ActionEvent e){
+      label.setLocation(label.getX()-10, label.getY());
     }
   }
 
-  public void keyReleased(KeyEvent e){
-    //System.out.println("You released key character: " + e.getKeyChar());
-    System.out.println("You released key code: " + e.getKeyCode());
+  public class RightAction extends AbstractAction {
+    public void actionPerformed(ActionEvent e){
+      label.setLocation(label.getX()+10, label.getY());
+    }
   }
 }
